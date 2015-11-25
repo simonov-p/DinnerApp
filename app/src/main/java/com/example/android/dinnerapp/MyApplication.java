c4c29b52eded430f6108f566ead1356af74b5062
Petr Simonov <simonov-p@ya.ru>
2015-11-25 07:54:25 +0300

    code create Tracker

--- app/src/main/java/com/example/android/dinnerapp/MyApplication.java
+++ app/src/main/java/com/example/android/dinnerapp/MyApplication.java
@@ -2,9 +2,24 @@ package com.example.android.dinnerapp;
 
 import android.app.Application;
 
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Tracker;
+
 /**
  * Created by petr on 25-Nov-15.
  */
 public class MyApplication extends Application {
+    public Tracker mTracker;
 
+    public void startTracking(){
+        if (mTracker == null) {
+            GoogleAnalytics googleAnalytics = GoogleAnalytics.getInstance(this);
+            mTracker = googleAnalytics.newTracker(R.xml.track_app);
+            googleAnalytics.enableAutoActivityReports(this);
+        }
+    }
+    public Tracker getmTracker() {
+        startTracking();
+        return mTracker;
+    }
 }
